e09bf9e1bd714159daf22566bdc2d6057ff4668e
RaffaelSilvaDev <rfsrafaelsilva@icloud.com>
2022-09-29 21:22:43 -0300

    Aula de Sintaxe e Indentação c#

--- Fundamentos/Program.cs
+++ Fundamentos/Program.cs
@@ -1,3 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Exemplo!");
+﻿using Fundamentos.Models;
+
+
+DateTime dataAtual = DateTime.Now; //Hora atual
+DateTime dataAtual1 = DateTime.Now.AddDays(5);
+Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm")); // imprime somente o for descrito no Tostring
+Console.WriteLine(dataAtual1);
+
+
+
+
+// string apresentacao = "Olá, seja Bem vindo..";
+
+// int quantidade = 1;
+// quantidade = 10; // subescreve primeiro valor de int, não usar o int para subescrever 
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;  // valores monetarios
+
+// bool condicao = true;
+
+
+
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variavel quantide: " + quantidade);
+// Console.WriteLine("Valor da variavel altura: " + altura.ToString("0.00")); // tratamento decimal
+// Console.WriteLine("Valor da variavel preço: " + preco);
+// Console.WriteLine("Valor da variavel condição: " + condicao);
+
+
+
+
+// //Pessoa pessoa1 = new Pessoa();
+
+// //pessoa1.Nome = "Rafael";
+// //pessoa1.Idade = 37;
+// //pessoa1.NomeRepresentanteLegal = "João";
+// pessoa1.Apresentar();
+
+
+
